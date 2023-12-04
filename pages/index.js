4c8b3acc650878961e1c1626256852f0f4e826c6
purplefade <tarovykanton@gmail.com>
2023-12-04 20:29:27 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,17 @@
 import Head from "next/head";
 import {
-  AiFillTwitterCircle,
   AiFillLinkedin,
-  AiFillYoutube,
+  AiFillGithub,
 } from "react-icons/ai";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
-import deved from "../public/dev-ed-wave.png";
+import antonpc from "../public/antonpc.webp";
 import code from "../public/code.png";
-import design from "../public/design.png";
+import customSolutions from "../public/design.png";
 import consulting from "../public/consulting.png";
 import Image from "next/image";
-import web1 from "../public/web1.png";
-import web2 from "../public/web2.png";
-import web3 from "../public/web3.png";
-import web4 from "../public/web4.png";
-import web5 from "../public/web5.png";
-import web6 from "../public/web6.png";
+import bose from "../public/bose_landing.jpg";
+import mybike from "../public/mybike_landing.jpg"
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
@@ -31,18 +26,20 @@ export default function Home() {
       <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between dark:text-white">
-            <h1 className="font-burtons text-xl">developedbyed</h1>
+            <h1 className="font-burtons text-xl">developedbyanton</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill
                   onClick={() => setDarkMode(!darkMode)}
-                  className=" cursor-pointer text-2xl"
+                  className=" cursor-pointer text-2xl "
                 />
               </li>
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="#"
+                  target='_blank'
+                  rel="noreferrer"
+                  href="https://drive.google.com/file/d/1iPzayp3gGzcUUfSYqjd9k7i9iGitvOlw/view?usp=sharing"
                 >
                   Resume
                 </a>
@@ -51,84 +48,123 @@ export default function Home() {
           </nav>
           <div className="text-center p-10 py-10">
             <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
-              Dimitri Marco
+              Anton Tarovyk
             </h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
-              Developer and designer.
+              Frontend Developer.
             </h3>
-            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-              Freelancer providing services for programming and design content
-              needs. Join me down below and let's get cracking!
+            <p className='text-md py-5 leading-8 text-gray-800 dark:text-gray-200 md:text-xl max-w-xl mx-auto'>
+              I&apos;m 19yo Frontend developer working with React, Redux,
+              RestAPI used with TypeScript.
             </p>
-            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
-              <AiFillTwitterCircle />
-              <AiFillLinkedin />
-              <AiFillYoutube />
+            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400 ">
+              <a
+                href='https://www.linkedin.com/in/anton-tarovyk-a38b42242/'
+                target='_blank'
+                rel="noreferrer"
+                className='hover:text-gray-200 transition duration-200 ease-in-out'
+              >
+                <AiFillLinkedin />
+              </a>
+              <a
+                href='https://github.com/purpleFade'
+                target='_blank'
+                rel="noreferrer"
+                className='hover:text-gray-200 transition duration-200 ease-in-out'
+              >
+                <AiFillGithub />
+              </a>
             </div>
             <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
-              <Image src={deved} layout="fill" objectFit="cover" />
+              <Image src={antonpc} layout="fill" objectFit="cover" alt='Anton is working'/>
             </div>
           </div>
         </section>
         <section>
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
+              Create custom web applications for businesses or individuals,
+              leveraging your expertise in frontend development to build
+              <span className='text-teal-500'>responsive</span> and{' '}
+              <span className='text-teal-500'>efficient</span> user interfaces.
+              Create and launch your own apps or tools that utilize React,
+              TypeScript, Redux, and REST APIs. It could be anything from
+              productivity tools to niche applications that solve{' '}
+              <span className='text-teal-500'>specific</span> problems.
             </p>
           </div>
           <div className="lg:flex gap-10">
             <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
-              <Image src={design} width={100} height={100} />
+              <Image src={customSolutions} width={100} height={100} alt='custom solutions'/>
               <h3 className="text-lg font-medium pt-8 pb-2  ">
-                Beautiful Designs
+                Custom Solutions
               </h3>
               <p className="py-2">
-                Creating elegant designs suited for your needs following core
-                design theory.
+                {' '}
+                Offer tailored solutions to businesses that require specific
+                functionalities or customizations in their applications,
+                utilizing your expertise in frontend technologies.
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+              <h4 className="py-4 text-teal-600">Technologies I use</h4>
+              <p className='text-gray-800 py-1'>TypeScript</p>
+              <p className='text-gray-800 py-1'>ReactJS</p>
+              <p className='text-gray-800 py-1'>Redux</p>
+              <p className='text-gray-800 py-1'>RestAPI</p>
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={code} width={100} height={100} />
+              <Image src={code} width={100} height={100} alt='code'/>
               <h3 className="text-lg font-medium pt-8 pb-2 ">
                 Code your dream project
               </h3>
               <p className="py-2">
-                Do you have an idea for your next great website? Let's make it a
+                Do you have an idea for your next great website? Let&apos;s make it a
                 reality.
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
+              <h4 className="py-4 text-teal-600">Coding Tools I Use</h4>
+              <p className="text-gray-800 py-1">VS Code</p>
               <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
               <p className="text-gray-800 py-1">Indesign</p>
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={consulting} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
+              <Image src={consulting} width={100} height={100} alt='consulting'/>
+              <h3 className="text-lg font-medium pt-8 pb-2 ">Work in team</h3>
               <p className="py-2">
-                Are you interested in feedback for your current project? I can
-                give you tips and tricks to level it up.
+                Collaboration fuels innovation.
+                Let&apos;s join forces and amplify your project&apos;s potential through combined expertise and teamwork.
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+              <h4 className="py-4 text-teal-600">Contact me</h4>
+              <p className="text-gray-800 py-1 ">
+                <a
+                  target='_blank'
+                  rel='noreferrer'
+                  href='mailto:tarovykanton@gmail.com'
+                  className=' hover:text-teal-400 transition duration-200 ease-in-out'
+                >
+                  Email
+                </a>
+              </p>
+              <p className="text-gray-800 py-1">
+                <a
+                  target='_blank'
+                  rel='noreferrer'
+                  href='https://www.linkedin.com/in/anton-tarovyk-a38b42242/'
+                  className=' hover:text-teal-400 transition duration-200 ease-in-out'
+                >
+                  Linkedin
+                </a>
+              </p>
+              <p className="text-gray-800 py-1">
+                <a
+                  target='_blank'
+                  rel='noreferrer'
+                  href='t.me/@refuelingkid'
+                  className=' hover:text-teal-400 transition duration-200 ease-in-out'
+                >
+                  Telegram
+                </a>
+              </p>
             </div>
           </div>
         </section>
@@ -136,72 +172,47 @@ export default function Home() {
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
+              Since the beginning of my journey as a frontend developer, I have done work for 
               <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
               and collaborated with talanted people to create digital products
-              for both business and consumer use.
+              for consumer use.
             </p>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
+              I offer from programming services.
             </p>
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
             <div className="basis-1/3 flex-1 ">
-              <Image
+              <a
+                href='https://purplefade.github.io/layout_landing-page/'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <Image
                 className="rounded-lg object-cover"
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
-                src={web1}
-              />
+                src={mybike}
+                alt='MyBiKE Landing Page'
+                />
+              </a>
             </div>
             <div className="basis-1/3 flex-1">
-              <Image
+              <a
+                href='https://purplefade.github.io/layout_miami/'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <Image
                 className="rounded-lg object-cover"
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
-                src={web2}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web3}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web4}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web5}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web6}
-              />
+                src={bose}
+                alt='Bose Landing Page'
+                />
+              </a>
             </div>
           </div>
         </section>
